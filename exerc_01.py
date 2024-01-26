caf7a63094ec11eeac49924692022ebaee883eb1
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2024-01-26 12:05:15 -0300

    Add files via upload

---
+++ exerc_01.py
@@ -0,0 +1,7 @@
+print("--- Cálculo da Área do Trapézio ---")
+base_maior = float(input("Informe o valor da Base Maior: "))
+base_menor = float(input("Informe o valor da Base Menor: "))
+altura = float(input("Informe o valor da Altura: "))
+
+area_do_trapezio = ((base_maior * base_menor)*altura)/2
+print("Área do Trapézio: ",area_do_trapezio)
---
+++ exerc_02.py
@@ -0,0 +1,6 @@
+import math
+
+print("=== Cálculo Areá do Circulo ===")
+raio = float(input("Informe o raio: "))
+area_circulo = (math.pi * (raio**2))
+print("Área do Circulo: ", round(area_circulo,2))
